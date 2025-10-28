cf6fb2f5b50a631025076a45869d153456d3f757
Sreenath T S <sreenathts655@gmail.com>
2025-10-28 18:13:31 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,27 @@
+  GNU nano 8.0                                                                                                      string1.l                                                                                                               
+%{
+#include<stdio.h>
+int i;
+%}
+%%
+[a-zA-Z]*       { for(i=0;i<yyleng;i++)
+{
+if((yytext[i]=='a') && (yytext[i+1]=='b') && (yytext[i+2]=='c'))
+{
+yytext[i]='A';
+yytext[i+1]='B';
+yytext[i+2]='C';
+}
+}
+printf("%s",yytext);
+}
+%%
+void main()
+{
+yylex();
+}
+int yywrap()
+{
+return 1;
+}
+
